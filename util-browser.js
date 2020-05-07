50e2cb6053f5f7e8b56435fb20f74df5cc8ff716
David I. Lehn <dlehn@digitalbazaar.com>
2020-05-07 12:47:06 -0400

    Fix empty hex string decoding.

--- util-browser.js
+++ util-browser.js
@@ -14,5 +14,8 @@ export function bytesToHex(bytes) {
 /* eslint-disable-next-line max-len */
 // https://stackoverflow.com/questions/43131242/how-to-convert-a-hexadecimal-string-of-data-to-an-arraybuffer-in-javascript
 export function bytesFromHex(hex) {
+  if(hex.length === 0) {
+    return new Uint8Array();
+  }
   return new Uint8Array(hex.match(/[\da-f]{2}/gi).map(h => parseInt(h, 16)));
 }
